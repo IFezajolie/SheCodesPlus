4340e98101efcbd4fd6285de8d8edf2061db6af4
IFezajolie <119081759+IFezajolie@users.noreply.github.com>
2023-12-03 23:52:30 +0200

    Updated the UI and the functionality

--- src/index.js
+++ src/index.js
@@ -6,23 +6,68 @@ const timeOutput = document.querySelector(".time");
 const temp = document.querySelector(".temp");
 
 let cityInput = "Kigali";
-// Adding event listeners to the form
+
+// Function to get weather emoji based on temperature
+function getWeatherEmoji(temperature) {
+  if (temperature > 0 && temperature <= 9) {
+    return "❄️"; // Cold, snowy weather
+  } else if (temperature > 9 && temperature <= 19) {
+    return "☁️"; // Cool or mild weather
+  } else {
+    return "🌥️"; // Warm or hot weather
+  }
+}
+
+// Function to fetch data
+function fetchWeatherData() {
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=cade3c69512a480abfd145701231911&q=${cityInput}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Get weather emoji based on temperature
+      const emoji = getWeatherEmoji(data.current.temp_c);
+
+      // Update the temperature display with the emoji
+      temp.innerHTML = `${emoji} ${data.current.temp_c}°C`;
+
+      // Getting the date and time of the city
+      const date = data.location.localtime;
+      const y = parseInt(date.substr(0, 4));
+      const m = parseInt(date.substr(5, 2));
+      const d = parseInt(date.substr(8, 2));
+      const time = date.substr(11);
+
+      // Re-formatting the page to make it more appealing
+      const dateOutput = document.querySelector(".date");
+      dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`;
+      if ((time > "00") & (time < 12)) {
+        timeOutput.innerHTML = time + " AM";
+      } else {
+        timeOutput.innerHTML = time + " PM";
+      }
+
+      // Add the name of the searched city to the page
+      nameOutput.innerHTML = data.location.name;
+    })
+    .catch((error) => {
+      alert("City not found. Please try again.");
+    });
+}
+
+// Event listener for form submission
 form.addEventListener("submit", (e) => {
-  // throw an error if the input field is empty
   if (search.value.length == 0) {
     alert("Please type in a city name");
   } else {
-    // We should change from our default city to the city in search bar
     cityInput = search.value;
-    // Then we should call a function to display all weather details about this city
     fetchWeatherData();
-    // Remove all the text that were searched from the input field
     search.value = "";
   }
-  // Preventing the default behaviour of the form
   e.preventDefault();
 });
 
+// Function to get the day of the week
 function dayOfTheWeek(day, month, year) {
   const date = new Date(year, month - 1, day);
   const days = [
@@ -32,52 +77,10 @@ function dayOfTheWeek(day, month, year) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   return days[date.getDay()];
 }
 
-// Function to fetch data
-function fetchWeatherData() {
-  // Fetching the data, and adding them dynamically with template literals
-  fetch(
-    `https://api.weatherapi.com/v1/current.json?key=cade3c69512a480abfd145701231911&q=${cityInput}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      // let's start with adding the temperature
-      temp.innerHTML = "☀️" + data.current.temp_c + "&#176" + "C";
-
-      // Getting the date and time of the city, extracted as day, month, year, and time
-
-      const date = data.location.localtime;
-      const y = parseInt(date.substr(0, 4));
-      const m = parseInt(date.substr(5, 2));
-      const d = parseInt(date.substr(8, 2));
-      const time = date.substr(11);
-
-      // Re-formatting the page to make it more appealing with the format of 17:53 - Friday 9, 10 2021
-      const dateOutput = document.querySelector(".date");
-      // const dayOfWeek = dayOfTheWeek(d, m, y);
-      dateOutput.innerHTML = `${dayOfTheWeek(d, m, y)} ${d}, ${m} ${y}`;
-      if ((time > "00") & (time < 12)) {
-        timeOutput.innerHTML = time + " AM";
-      } else {
-        timeOutput.innerHTML = time + " PM";
-      }
-
-      // Adding the name of the searched city to the page
-      nameOutput.innerHTML = data.location.name;
-
-      // Setting default time of the day
-      let timeOfDay = "day";
-
-      // Getting the unique id for each weather condition
-      const code = data.current.condition.code;
-    })
-    .catch((error) => {
-      alert("City not found. Please try again.");
-    });
-}
-
+// Initial fetch on page load
 fetchWeatherData();
